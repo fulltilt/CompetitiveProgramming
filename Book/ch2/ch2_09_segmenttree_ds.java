3808036b3de6c934770fc081aa946d0a1a294d01
Dave D <dave.doria@gmail.com>
2015-01-02 11:28:16 -0800

    updated notes for Segment Tree

--- Book/ch2/ch2_09_segmenttree_ds.java
+++ Book/ch2/ch2_09_segmenttree_ds.java
@@ -1,20 +1,25 @@
 import java.util.*;
 
+/* Used to efficiently answer dynamic range queries. If the data is static, there's a more efficient dynamic programming solution
+*/
 class SegmentTree {         // the segment tree is stored like a heap array
   private int[] st, A;
   private int n;
   private int left (int p) { return p << 1; } // same as binary heap operations
   private int right(int p) { return (p << 1) + 1; }
 
+  // p: index in 'st' (index 0 is unused)
   private void build(int p, int L, int R) {
-    if (L == R)                            // as L == R, either one is fine
+    if (L == R) {                           // as L == R, either one is fine
       st[p] = L;                                         // store the index
-    else {                                // recursively compute the values
+    } else {                                // recursively compute the values
       build(left(p) , L              , (L + R) / 2);
       build(right(p), (L + R) / 2 + 1, R          );
-      int p1 = st[left(p)], p2 = st[right(p)];
+      int p1 = st[left(p)], 
+          p2 = st[right(p)];
       st[p] = (A[p1] <= A[p2]) ? p1 : p2;
-  } }
+    } 
+  }
 
   private int rmq(int p, int L, int R, int i, int j) {          // O(log n)
     if (i >  R || j <  L) return -1; // current segment outside query range
@@ -26,7 +31,7 @@ class SegmentTree {         // the segment tree is stored like a heap array
 
     if (p1 == -1) return p2;   // if we try to access segment outside query
     if (p2 == -1) return p1;                               // same as above
-    return (A[p1] <= A[p2]) ? p1 : p2; }          // as as in build routine
+    return (A[p1] <= A[p2]) ? p1 : p2; }          // as in build routine
 
   private int update_point(int p, int L, int R, int idx, int new_value) {
     // this update code is still preliminary, i == j
@@ -57,15 +62,21 @@ class SegmentTree {         // the segment tree is stored like a heap array
 
   public SegmentTree(int[] _A) {
     A = _A; n = A.length;                   // copy content for local usage
-    st = new int[4 * n];
-    for (int i = 0; i < 4 * n; i++) st[i] = 0; // create vector with length `len' and fill it with zeroes
+    st = new int[4 * n];                    // 4 * n to create a large enough array of zeroes
+    for (int i = 0; i < 4 * n; i++) {
+      st[i] = 0; // create vector with length `len' and fill it with zeroes
+    }
+
     build(1, 0, n - 1);                                  // recursive build
   }
 
-  public int rmq(int i, int j) { return rmq(1, 0, n - 1, i, j); } // overloading
+  public int rmq(int i, int j) {  // overloading
+    return rmq(1, 0, n - 1, i, j); 
+  } 
 
   public int update_point(int idx, int new_value) {
-    return update_point(1, 0, n - 1, idx, new_value); }
+    return update_point(1, 0, n - 1, idx, new_value); 
+  }
 }
 
 class ch2_09_segmenttree_ds {
@@ -96,3 +107,31 @@ class ch2_09_segmenttree_ds {
     System.out.printf("RMQ(4, 5) = %d\n", st.rmq(4, 5));            // 5->4
   }
 }
+
+/*
+A is {18,17,13,19,15,11,20}
+st = [0, 5, 2, 5, 1, 2, 5, 6, 0, 1, 2, 3, 4, 5]
+index 0  1  2  3  4  5  6  7  8  9  10 11 12 13
+
+                                         1:[0,6]=5
+
+                  2:[0,3]=2                                        3:[4,6]=5  
+ 
+      4:[0,1]=1               5:[2,3]=2                6:[4,5]=5               7:[6,6]=6
+
+8:[0,0]=0  9:[1,1]=1   10:[2,2]=2  11:[3,3]=3   12:[4,4]=4  13:[5,5]=5
+
+if A[5] is updated from 11 to 100: {18,17,13,19,15,100,20}
+st = [0, 2, 2, 4, 1, 2, 4, 6, 0, 1, 2, 3, 4, 5]
+index 0  1  2  3  4  5  6  7  8  9  10 11 12 13
+
+                                         1:[0,6]=2
+
+                  2:[0,3]=2                                        3:[4,6]=4  
+ 
+      4:[0,1]=1               5:[2,3]=2                6:[4,5]=4               7:[6,6]=6
+
+8:[0,0]=0  9:[1,1]=1   10:[2,2]=2  11:[3,3]=3   12:[4,4]=4  13:[5,5]=5
+
+*/
+
